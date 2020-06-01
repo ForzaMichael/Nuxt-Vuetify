6a900357bbf2c416310aacb69db1e37d92de986b
李琪 <liqi_zb@cmos.chinamobile.com>
2020-06-01 20:34:49 +0800

    login

---
+++ layouts/login-layout.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+      app
+      absolute
+      color="#43a047"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-5"
+      scroll-threshold="500"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-row align="center" justify="center">
+        <nuxt />
+      </v-row>
+    </v-content>
+  </v-card>
+</template>
